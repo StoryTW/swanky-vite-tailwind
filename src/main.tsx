3d97e4d00638e7c913ea63796b5f9e4e90de1469
parienko_sergej <parienko_sergej@enot.work>
2023-09-17 22:58:01 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { NextUIProvider } from '@nextui-org/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
